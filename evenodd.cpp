7cd7334960e7962d4dcb08db833e479046d6b242
mathadjyoti112-lab <mathadjyoti112@gmail.com>
2026-08-17 16:32:48 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;
+std::cout<<"enter the number: ";
+std::cin>>number;
+if(number%2==0)
+{
+std::cout<<number<<"is even"<<std::endl;
+}
+else{
+std::cout<<number<<"is odd"<<std::endl;
+}
+return 0;
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
