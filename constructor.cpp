fb0f024c464c82a28cc92ff5107fffbfebd45f7d
mathadjyoti112-lab <mathadjyoti112@gmail.com>
2026-08-17 16:30:43 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,61 @@
+
+
+
+
+
+#include<iostream>
+using namespace std;
+class A
+{
+public:
+A()
+
+{
+cout<<" constructor is called"<<endl;
+}
+~A()
+{
+cout<<"destuctor is called"<<endl;
+}
+};
+int main()
+{A obj1;
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
