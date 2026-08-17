4eb668b6eb83172ea432ceba39bf3ed3e0b5565c
mathadjyoti112-lab <mathadjyoti112@gmail.com>
2026-08-17 16:34:18 +0530

    Add files via upload

---
+++ nestedclass.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class A{
+public:
+class B {
+private:
+void getdata(int n)[
+num=n;
+void putdata(){
+cout<<"the number is"<<num;
+int main()
+{
+cout<<Nested class in c++"<<endl;
+A::B obj;
+obj getdata(9);
+obj putdata();
+return 0;
+} 
+
+
