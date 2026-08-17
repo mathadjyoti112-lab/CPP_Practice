3a2d9c1dab9541db8a425e797e84425a87c928c0
mathadjyoti112-lab <mathadjyoti112@gmail.com>
2026-08-17 16:27:05 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=9;
+int b=2;
+int c=a+b;
+cout<<"the sum of"<<a<<"and"<<b<<"is"<<c<<endl;
+c=a-b;
+cout<<"subtraction of"<<a<<"and"<<b<<"is"<<c<<endl;
+c=a*b;
+cout<<"multiplication of"<<a<<"and"<<b<<"is"<<c<<endl;
+c=a/b;
+cout<<"division of"<<a<<"and"<<b<<"is"<<c<<endl;
+return 0;
+}
